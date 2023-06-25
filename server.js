8d2c6a22276228cf42d110c95f7fb21ebadf3fce
Ibrokhimbek <ibrokhim1348@gmail.com>
2023-06-25 14:56:18 +0500

    db connection

--- server.js
+++ server.js
@@ -21,19 +21,19 @@ fastify.register(require("./routes/adminRoutes"), { prefix: "/admin" });
 fastify.register(require("./routes/employerRoutes"), { prefix: "/employer" });
 
 //? Database setup
-mongoose
-  .connect("mongodb://127.0.0.1:27017/iDev", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+// mongoose
+//   .connect("mongodb://127.0.0.1:27017/iDev", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("MongoDB connected"))
+//   .catch((err) => console.error("MongoDB connection error:", err));
 
 //* Database connection
-// mongoose.set("strictQuery", true);
-// mongoose.connect(process.env.DB_CONNECTION).then(() => {
-//   console.log("Connected");
-// });
+mongoose.set("strictQuery", true);
+mongoose.connect(process.env.DB_CONNECTION).then(() => {
+  console.log("Connected");
+});
 
 fastify.addHook("onClose", async () => {
   await mongoose.connection.close();
